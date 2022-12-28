0b2e09be3b976c1851258eeb1e1d877356e059b2
berlin <0xb3rlin@gmail.com>
2022-12-28 16:50:59 -0600

    change projects section to flex wrap (grid before)

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ export const Projects = () => {
     <ProjectCard key={project.id} project={project} />
   ));
   return (
-    <article className='flex flex-wrap justify-items-center gap-4 px-4 py-2 md:grid md:grid-cols-2 md:place-content-center md:p-6 '>
+    <article className='flex flex-wrap justify-items-center gap-4 px-4 py-2  md:place-content-center md:p-6 '>
       {projectsList}
     </article>
   );
